fba921cc21d9d73f71a7966888f874a78b3315f8
Piers Stuart-Kelso <piers.stuart-kelso@sky.uk>
2018-06-27 12:38:49 +0100

    adds grass animations

---
+++ Assets/StepOnGrass.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepOnGrass : MonoBehaviour {
+
+	public Sprite grassNormal;
+	public Sprite grassSteppedOn;
+	public GameObject player;
+
+    private SpriteRenderer grassRenderer;
+	
+	// Use this for initialization
+	void Start () {
+        grassRenderer = gameObject.GetComponent<SpriteRenderer>();
+	}
+	
+    void OnTriggerStay2D() {
+		grassRenderer.sprite = grassSteppedOn;
+		if(transform.position.y < (player.transform.position.y - 0.15f)) {
+			grassRenderer.sortingOrder = 3;
+		} else {
+			grassRenderer.sortingOrder = 1;
+		}
+    }
+
+	void OnTriggerExit2D() {
+		Debug.Log("player leaves");
+		grassRenderer.sprite = grassNormal;
+		grassRenderer.sortingOrder = 1;
+    }
+}
