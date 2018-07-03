851aa1d99cfdd91a9d24a468b76af62e51661daa
Piers Stuart-Kelso <piers.stuart-kelso@sky.uk>
2018-07-03 16:48:36 +0100

    ahhhhhhh

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInteraction : MonoBehaviour {
+	public float pickupRange;
+	public Text infoText;
+	public TextBoxRenderer textBox;
+
+	private RaycastHit2D[] results;
+	private Collider2D playerCollider;
+
+	// Use this for initialization
+	void Start () {
+		playerCollider = gameObject.GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckForInteraction();
+	}
+
+	void CheckForInteraction() {
+		if(Input.GetKeyDown(KeyCode.E)) {
+			Vector2 playerDirection = PlayerMobility.GetPlayerDirection();
+			Vector2 playerPosition = new Vector2(
+				transform.position.x + playerCollider.offset.x,
+				transform.position.y + playerCollider.offset.y
+			);
+
+			RaycastHit2D hit = Physics2D.Raycast(playerPosition, playerDirection, pickupRange);
+
+			if (hit.collider != null) {
+				string colliderTag = hit.collider.tag;
+				switch (colliderTag) {
+					case "Item":
+						PickupItem(hit.collider.gameObject);
+						break;
+					case "Water":
+						PlayerSurf(hit.collider.gameObject, playerDirection);
+						break;
+					case "Sign":
+						ReadSign(hit.collider.gameObject.GetComponent<Sign>());
+						break;
+					default:
+						Debug.Log("Collider has no Interaction options");
+						break;
+				}
+			}
+		}
+    }
+
+	void PickupItem(GameObject itemObj) {
+		ItemIdentifier item = itemObj.GetComponent<ItemIdentifier>();
+
+		bool itemObtained = PlayerInventory.AddItemToInventory(item.itemName);
+		if(itemObtained) {
+			Destroy(itemObj);
+			if(item.itemName != "Sword") {
+				TextBoxRenderer.DisplayTextOnUI("Obtained item: " + item.itemName, false);
+			} else {
+				TextBoxRenderer.DisplayTextOnUI("It's dangerous to go alone! Take this!", false);
+			}
+		} else {
+			TextBoxRenderer.DisplayTextOnUI("Inventory is full!", false);	
+		}
+	}
+
+	void PlayerSurf(GameObject water, Vector2 playerDirection) {
+		List<string> inventory = PlayerInventory.GetPlayerInventory();
+
+		if(inventory.Contains("Surf")) {
+			TextBoxRenderer.DisplayTextOnUI("You use Surf!", false);
+			transform.position += new Vector3(playerDirection.x, playerDirection.y, 0);
+		} else {
+			TextBoxRenderer.DisplayTextOnUI("You need Surf in order to cross water!", false);
+		}
+	}
+
+	void ReadSign(Sign signComponent) {
+		string signText = signComponent.GetSignText();
+		TextBoxRenderer.DisplayTextOnUI(signText, true);
+	}
+
+}
--- Assets/Scripts/PlayerMobility.cs
+++ Assets/Scripts/PlayerMobility.cs
@@ -7,27 +7,61 @@ public class PlayerMobility : MonoBehaviour {
 	//Inspector Variables
     public float playerSpeed; //speed player moves
     public GameObject camera;
+<<<<<<< Updated upstream
+=======
+
+    public Vector2 knockbackDirection;
+    public float knockbackMultiplier;
+    public float knockbackCount;
+
+    public float attackCount;
+
+    public bool isInvincible;
+
+>>>>>>> Stashed changes
     private Vector3 offset;
     private Vector3 spawnPoint;
+
     private Animator playerAnimator;
+<<<<<<< Updated upstream
+=======
+    private Rigidbody2D playerRB;
+    private SpriteRenderer playerSprite;
+
+>>>>>>> Stashed changes
     private static float faceX;
     private static float faceY;
 
     void Start () {
         SetSpawnPoint();
+
         playerAnimator = GetComponent<Animator>();
+<<<<<<< Updated upstream
+=======
+        playerRB = gameObject.GetComponent<Rigidbody2D>();
+        playerSprite = gameObject.GetComponent<SpriteRenderer>();
+
+>>>>>>> Stashed changes
         playerAnimator.Play("Idle");
     }
    
     void Update () {
-        MoveForward();
+       if(!TextBoxRenderer.persistText) MoveForward();
     }
 
     void SetSpawnPoint() {
+<<<<<<< Updated upstream
         int returningFromRoom = PlayerPrefs.GetInt("returningFromRoom"); //1 = true, 0 = false
         spawnPoint = returningFromRoom == 1
             ? new Vector3(12.5f, 8.3f, 0)
             : new Vector3(0, 0, 0);
+=======
+        // int returningFromScene = PlayerPrefs.GetInt("returningFromScene"); //1 = true, 0 = false
+        Vector3 savedSpawnPoint = new Vector3(PlayerPrefs.GetFloat("spawnX"), PlayerPrefs.GetFloat("spawnY"), PlayerPrefs.GetFloat("spawnZ"));
+
+        spawnPoint = savedSpawnPoint;
+
+>>>>>>> Stashed changes
         offset = camera.transform.position - transform.position;
         transform.position = spawnPoint;
         camera.transform.position = spawnPoint + offset;
@@ -43,12 +77,27 @@ public class PlayerMobility : MonoBehaviour {
             faceY = yInput;
             playerAnimator.SetFloat("FaceX", faceX);
             playerAnimator.SetFloat("FaceY", faceY);
-            playerAnimator.Play("Moving");
+            if(attackCount <= 0) playerAnimator.Play("Moving");
         } else {
-            playerAnimator.Play("Idle");
+            if(attackCount <= 0) playerAnimator.Play("Idle");
         }
 
+<<<<<<< Updated upstream
         transform.Translate(xInput * playerSpeed, yInput * playerSpeed, 0);
+=======
+        // transform.Translate(xInput * playerSpeed * Time.deltaTime, yInput * playerSpeed * Time.deltaTime, 0);
+        if(knockbackCount <= 0 && attackCount <= 0) {
+            playerSprite.color = new Color(255, 255, 255);
+            playerRB.velocity = new Vector2(xInput, yInput) * playerSpeed; //removes vibrations
+        } else if (attackCount <=0 && knockbackCount >= 0) {
+            playerSprite.color = new Color(255, 0, 0);
+            playerRB.velocity = knockbackDirection * knockbackMultiplier;
+            knockbackCount -= Time.deltaTime;
+        } else {
+            playerAnimator.Play("Attacking");
+            attackCount -= Time.deltaTime;
+        }
+>>>>>>> Stashed changes
     }
 
     public static Vector2 GetPlayerDirection() {
